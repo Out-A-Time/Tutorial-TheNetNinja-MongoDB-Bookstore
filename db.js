981af6c9522384d1d1ddeb3eb9528b5785f2647f
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2022-09-12 14:25:55 +0100

    Moved from local  MongoDB Compass to cloud  MongoDB Atlas

--- db.js
+++ db.js
@@ -1,11 +1,17 @@
 const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const mongoUSER = process.env.mongoPASSWORD;
+const mongoPASSWORD = process.env.mongoPASSWORD;
 
 let dbConnection;
+let uri = `mongodb+srv://Out-A-Time:${mongoPASSWORD}@mongodb-cluster0.99qmncj.mongodb.net/?retryWrites=true&w=majority`;
+// let uri ="mongodb+srv://Out-A-Time:xxx@mongodb-cluster0.99qmncj.mongodb.net/?retryWrites=true&w=majority";
 
 module.exports = {
   //Establish connection to DB
   connectToDB: (cb) => {
-    MongoClient.connect("mongodb://127.0.0.1:27017/bookstore")
+    // MongoClient.connect("mongodb://127.0.0.1:27017/bookstore")   LOCAL
+    MongoClient.connect(uri)
       .then((client) => {
         dbConnection = client.db();
         return cb();
